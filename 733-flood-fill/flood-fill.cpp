aa7960a1f37d86acb92aa6a056c927a537f95897
Naman Rao Ghorpade <Namanghorpade.28@gmail.com>
2025-07-09 19:29:24 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (26.00%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int starting_row, int starting_column, int new_color) {
+        //implement breath first seach in this.
+        int n= image.size();
+        int m = image[0].size();
+
+        queue<pair<int,int>> q;
+        int starting_color = image[starting_row][starting_column];
+        int delta_row[] = {0 , -1 , 0 ,1};
+        int delta_column []= {-1, 0 , 1, 0};
+        
+        if(starting_color == new_color){
+            return image;
+        }
+
+        if(starting_color != new_color){
+            image[starting_row][starting_column] = new_color;
+            q.push({starting_row , starting_column});
+            while(!q.empty()){
+                auto [i,j] = q.front();
+                q.pop();
+
+                for(int d = 0 ; d<4 ;d++){
+                    int neighbour_row = i + delta_row[d];
+                    int negihbour_column = j +delta_column[d];
+
+                    if(neighbour_row<n &&neighbour_row>=0 &&negihbour_column<m &&negihbour_column>=0 && image[neighbour_row][negihbour_column]==starting_color){
+                        image[neighbour_row][negihbour_column] = new_color;
+                        q.push({neighbour_row,negihbour_column});
+                    }
+                }
+            }
+        }
+    return image;
+    }
+};
