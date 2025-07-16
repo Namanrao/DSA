267bb7de4960f37e6c5806dd27416fc97f487fef
Naman Rao Ghorpade <Namanghorpade.28@gmail.com>
2025-07-16 18:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (39.18%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-    ListNode* reversell(ListNode* start, ListNode* end) {
-        ListNode* prev = NULL;
-        ListNode* curr = start;
-        ListNode* stop = end->next;
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head) return nullptr;
 
-        while (curr != stop) {
-            ListNode* forward = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = forward;
+        ListNode* tail = head;
+        for (int i = 0; i < k; ++i) {
+            if (!tail) return head;
+            tail = tail->next;
         }
-        return prev;
+
+        ListNode* newHead = reverse(head, tail);
+        head->next = reverseKGroup(tail, k);
+        return newHead;
     }
 
-    ListNode* reverseKGroup(ListNode* head, int k) {
-        if (head == NULL || k == 1)
-            return head;
-
-        ListNode* dummy = new ListNode(-1);
-        dummy->next = head;
-        ListNode* beforeStart = dummy;
-        ListNode* end = head;
-
-        while (true) {
-            // Find the end of the k-group
-            for (int i = 1; i < k && end != NULL; i++) {
-                end = end->next;
-            }
-            if (end == NULL) break;
-
-            ListNode* start = beforeStart->next;
-            ListNode* nextGroup = end->next;
-
-            // Reverse this k-group
-            ListNode* newHead = reversell(start, end);
-            beforeStart->next = newHead;
-            start->next = nextGroup;
-
-            // Move pointers
-            beforeStart = start;
-            end = start->next;
+private:
+    ListNode* reverse(ListNode* cur, ListNode* end) {
+        ListNode* prev = nullptr;
+        while (cur != end) {
+            ListNode* next = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = next;
         }
-
-        return dummy->next;
+        return prev;
     }
 };
