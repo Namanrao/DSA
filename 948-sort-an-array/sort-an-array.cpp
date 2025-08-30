313ffc7cf17201e924ae19c87b21200859c605a3
Naman Rao Ghorpade <Namanghorpade.28@gmail.com>
2025-08-30 12:22:43 +0530

    Time: 91 ms (62.70%) | Memory: 99.3 MB (49.77%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,52 +1,23 @@
 class Solution {
 public:
-    
-    void merge(vector<int> &nums, int low , int mid , int high){
-        vector<int> temp;
-        int left = low;
-        int right = mid+1;
-        while(left<=mid && right<=high){
-            if(nums[left]<= nums[right]){
-                temp.push_back(nums[left]);
-                left++;
-            }else{
-                temp.push_back(nums[right]);
-                right++;
-            }
-        }
-        while(left<=mid){
-            temp.push_back(nums[left]);
-            left++;
-        }
-        while(right<=high){
-            temp.push_back(nums[right]);
-            right++;
-        }
-        for(int i = low ; i<=high ; i++){
-            nums[i] = temp[i-low]; //zero indexing
-        }
-    }
-
-    void mergeSort(vector<int> &nums, int low , int high){
-        // base case
-        if(low>=high){
-            return ;
-        }
-        int mid = ceil((low+high)/2);
-        // calling mergeSort for the first part.
-        mergeSort(nums, low , mid);
-        // calling mergeSort for the second part.
-        mergeSort(nums, mid +1 , high);
-        // calling merge function
-        merge(nums,low,mid,high);
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        // Implementing merge sort algorithm
         int n = nums.size();
-        int low = 0 ;
-        int high = n-1;
-        mergeSort(nums, low , high);
-        return nums;
+        int max_ele = *max_element(nums.begin() , nums.end());
+        int min_ele = *min_element(nums.begin() , nums.end());
+
+        unordered_map<int , int> mp;
+        for(int i :nums){
+            mp[i]++;
+        }
+
+        vector<int> result;
+         for(int i = min_ele ; i<=max_ele ; i++){
+            if(mp.find(i)!=mp.end()){
+                while(mp[i]--){
+                    result.push_back(i); 
+                }
+            }
+         }
+         return result;
     }
 };
