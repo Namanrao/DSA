f436651eebdbe647294d2cace7f3bdcc6e7f03cc
Naman Rao Ghorpade <Namanghorpade.28@gmail.com>
2025-09-23 12:06:29 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (47.03%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,30 +1,22 @@
 class Solution {
-    // memoization solution
 
-    public int solve(int [] nums , int index , int n , int[] dp ){
-        // base case
-        if(index == 0){
-            return nums[index];
+    public int solve(int[] nums){
+        int n = nums.length;
+        int [] dp = new int[n];
+        dp[0]  = nums[0];
+        for(int i =1 ; i< n; i++){
+            int pick = nums[i];
+            if(i>1 ){
+                pick+= dp[i-2];
+            }
+            int skip = dp[i-1] + 0 ;
+            dp[i] = Math.max(pick , skip);
         }
-        if(index<0){
-            return 0 ;
-        }
-        if(dp[index]!= -1 ){
-            return dp[index];
-        }
-        int pick = solve(nums, index -2 , n , dp) + nums[index];
-        int skip  = solve(nums, index -1 , n , dp) + 0;
-
-        return dp[index]  = Math.max(pick , skip);
+        return dp[n-1];
     }
 
     public int rob(int[] nums) {
-        int n = nums.length;
-        int index = n -1;
-        int [] dp = new int [n+1];
-        Arrays.fill(dp , -1);
-        int result = solve(nums , index , n , dp);
-        
-        return result;
+    int result = solve(nums);
+        return result ;
     }
 }
