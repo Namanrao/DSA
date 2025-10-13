5f4922c37a3a351ff21c68303d93323f281052b1
Naman Rao Ghorpade <Namanghorpade.28@gmail.com>
2025-10-13 13:56:52 +0530

    Time: 2 ms (43.90%) | Memory: 43.6 MB (24.90%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -16,27 +16,47 @@ public boolean isValid(String str) {
     return st.isEmpty(); 
 }
 
-
-    public void solve(List<String> main_list,String str , int opening , int closing){
-        // base condition
-        if(closing ==0 && opening == 0){
+    public void solve(List<String> main_list, String str , int opening , int closing , int balancer){
+        if(closing ==0 && opening ==0){
             main_list.add(str);
             return ;
         }
-        if(closing > 0 && opening>0 ){
-            // you have both the choices 
-            solve(main_list , str+ "(" , opening -1 , closing);
-            solve(main_list , str + ")" , opening , closing -1);
+        if(balancer==0){
+            // you have only option to open the pareenthesis 
+            if(opening> 0){
+                solve(main_list , str+ "(" , opening -1, closing , balancer+1);
+            }
+        }else if (balancer> 0){
+            // you have both the options 
+            if(opening > 0 ){
+                solve(main_list , str+ "(" , opening -1  ,closing , balancer+1);
+            }
+            if(closing > 0){
+                solve(main_list, str + ")" , opening , closing -1 , balancer -1);
+            }
         }
-        else if (closing > 0){
-            // you have one choice
-            solve(main_list, str+")" , opening, closing -1);
-        }
-        else if(opening>0){
-            // you still have one choice
-            solve(main_list , str+"(" , opening -1 , closing);
-        }
-    }   
+    }
+
+    // public void solve(List<String> main_list,String str , int opening , int closing){
+    //     // base condition
+    //     if(closing ==0 && opening == 0){
+    //         main_list.add(str);
+    //         return ;
+    //     }
+    //     if(closing > 0 && opening>0 ){
+    //         // you have both the choices 
+    //         solve(main_list , str+ "(" , opening -1 , closing);
+    //         solve(main_list , str + ")" , opening , closing -1);
+    //     }
+    //     else if (closing > 0){
+    //         // you have one choice
+    //         solve(main_list, str+")" , opening, closing -1);
+    //     }
+    //     else if(opening>0){
+    //         // you still have one choice
+    //         solve(main_list , str+"(" , opening -1 , closing);
+    //     }
+    // }   
 
     public List<String> generateParenthesis(int n) {
         // lets try to solve it using recursion 
@@ -44,14 +64,15 @@ public boolean isValid(String str) {
         String str = "";
         int opening = n ; 
         int closing = n ;
-        solve(main_list , str, n ,n);
-        List<String> new_list = new ArrayList<>();
+        int balancer = 0;
+        solve(main_list , str, n ,n,balancer);
+        // List<String> new_list = new ArrayList<>();
         // check for the invalid parenthesis,
-        for(int i=0 ; i<main_list.size() ; i++){
-            if(isValid(main_list.get(i))){
-                new_list.add(main_list.get(i));
-            }
-        }
-        return new_list;
+        // for(int i=0 ; i<main_list.size() ; i++){
+        //     if(isValid(main_list.get(i))){
+        //         new_list.add(main_list.get(i));
+        //     }
+        // }
+        return main_list;
     }
 }
