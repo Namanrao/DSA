89909cbf539b3e985f7b24866491dc4da202bcf8
Naman Rao Ghorpade <Namanghorpade.28@gmail.com>
2026-04-05 11:22:48 +0530

    Time: 1 ms (60.99%) | Memory: 10.4 MB (47.26%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x =0;
+        int y = 0;
+
+        for(char c:moves){
+            if(c =='U'){
+                y = y+1;
+            }else if(c =='D'){
+                y = y -1;
+            }else if(c=='L'){
+                x = x -1;
+            }else{
+                x = x +1;
+            }
+        }
+        if(x ==0 && y==0) return true;
+        return false;
+    }
+};
